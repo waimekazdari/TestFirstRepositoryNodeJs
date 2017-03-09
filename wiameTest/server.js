0eeb9fb76e1e380313155375a885dd2693c1a96b
wiame <kazdari.wiam@gmail.com>
2017-03-09 19:24:04 +0000

    some modifications

--- wiameTest/server.js
+++ wiameTest/server.js
@@ -1,8 +1,21 @@
 var express = require('express')
 var app = express()
-
+var body_parser=require('body-parser');
 app.get('/', function (req,res) {
   res.send('hello world')
 })
+
+// for send an other file , we have to use --dirname to recover our directory path
+app.use(body_parser.urlencoded({extended:false}));
+
+  app.get('/emps',function(req,res){
+  res.sendFile(__dirname+'/emp.html');
+})
+app.post(
+  '/emp',function (req,res) {
+    var empName=req.body.empName;
+    console.log(empName);
+  }
+)
 
 app.listen(3000)
